156f747387cc927af82a651b5bb527d9293c5c34
shourovhasan~ <shourovhasan001@gmail@.com>
2024-08-13 21:56:39 +0600

    project live link in firebase.jsx file  ~

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -16,3 +16,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+
+
+// live  project  link in below 
+//URL: https://react-dragon-news-9c90d.web.app
+
